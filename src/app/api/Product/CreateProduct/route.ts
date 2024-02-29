b501c03bd38fb347553e45d44b94d7fcadda7790
Anurag Sidhu <anuragsidhu72@gmail.com>
2024-02-29 11:23:15 +0530

    API CREATED

---
+++ src/app/api/Product/CreateProduct/route.ts
@@ -0,0 +1,37 @@
+
+import client from "@/libs/prismadb";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+    try {
+        const { name, category, image, price, description, video } = await request.json();
+
+        // Check if the provided productId exists before creating the review
+
+        const newReview = await client.product.create({
+            data: {
+                name, category, image, price, description, video
+            }
+        });
+
+        return NextResponse.json(newReview, { status: 201 });
+    } catch (error) {
+        console.error("Error creating review:", error);
+        return new NextResponse("Internal error", { status: 500 });
+    }
+}
+
+export async function GET(request: Request) {
+    try {
+
+
+        // Check if the provided productId exists before creating the review
+
+        const newReview = await client.product.findMany()
+
+        return NextResponse.json(newReview, { status: 201 })
+    } catch (error) {
+        console.error("Error creating review:", error);
+        return new NextResponse("Internal error", { status: 500 });
+    }
+}
